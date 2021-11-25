9171350ddd0b35e17b87a92107e0b790595633e8
Bruno Peixoto <brunolnetto@gmail.com>
2021-11-25 18:45:51 -0300

    feat - MB charts embbeding

--- node/index.js
+++ node/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const session = require("express-session");
 const jwt = require("jsonwebtoken");
+require("dotenv").config;
 
 const PORT = process.env["PORT"] ? parseInt(process.env["PORT"]) : 3001;
 
 // these should match the settings in your Metabase instance
-let MB_SITE_URL = "http://localhost:3000";
-let MB_EMBEDDING_SECRET_KEY = "c39fcfd02abd76b0ad200d1eada354f136c383638dcfc189253b9b8e0dd13e46";
+let MB_SITE_URL = "http://3.231.180.105:8080";
+let MB_EMBEDDING_SECRET_KEY = "bada589e31ea1d6c11695653952d66ea2d5cb0dde8e37da1970489bc0a4e616e";
+let manager = "Rafael Moreira";
+let MOCK_ID = 42;
+
 
 function checkAuth(req, res, next) {
     const userId = req.session.userId;
@@ -21,7 +25,7 @@ function checkAuth(req, res, next) {
 const DASHBOARD_ID = 2;
 
 if (!MB_EMBEDDING_SECRET_KEY) {
-  throw new Error("Please set MB_EMBEDDING_SECRET_KEY.");
+    throw new Error("Please set MB_EMBEDDING_SECRET_KEY.");
 }
 if (typeof DASHBOARD_ID !== "number" || isNaN(DASHBOARD_ID)) {
   throw new Error("Please set DASHBOARD_ID.");
@@ -59,19 +63,19 @@ app.get("/logout", (req, res) => {
 
 // authenticated routes
 
-app.get("/signed_chart/:id", checkAuth, (req, res) => {
-    const userId = req.session.userId;
-    const unsignedToken = {
-        resource: { question: 2 },
-        params: { person_id: userId },
-        exp: Math.round(Date.now() / 1000) + (10 * 60) // 10 minute expiration
-    };
+app.get("/signed_chart", checkAuth, (req, res) => { 
+    var payload = {
+                    resource: { question: 136 },
+                    params: {
+                                "gerente": manager
+                            },
+                    exp: Math.round(Date.now() / 1000) + (10 * 60) // 10 minute expiration
+                  };
+    var token = jwt.sign(payload, MB_EMBEDDING_SECRET_KEY);
 
-    // sign the JWT token with our secret key
-    const signedToken = jwt.sign(unsignedToken, MB_EMBEDDING_SECRET_KEY);
-    // construct the URL of the iframe to be displayed
-    const iframeUrl = `${MB_SITE_URL}/embed/question/${signedToken}`;
-    res.render("chart", { userId: req.params.id, iframeUrl: iframeUrl });
+    var iframeUrl = MB_SITE_URL + "/embed/question/" + token + "#bordered=true&titled=true";
+    console.log(iframeUrl);
+    res.render("chart", { userId: MOCK_ID, iframeUrl: iframeUrl });
 })
 
 app.get("/signed_dashboard/:id", checkAuth, (req, res) => {
